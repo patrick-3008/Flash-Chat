9526b8a8c261add440fd7256d07b5b19a520cc57
Patrick <patricknaashat@yahoo.com>
2023-10-05 13:09:16 +0300

    add login using firebase

--- Flash Chat/Controllers/LoginVC.swift
+++ Flash Chat/Controllers/LoginVC.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
 
@@ -6,7 +7,21 @@ class LoginVC: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
 
     @IBAction func loginPressed(_ sender: UIButton) {
-        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) {  authResult, error in
+                if let e = error {
+                    // Show alert dialog with the error
+                    print(e)
+                    let alert = UIAlertController.init(title: "Login Error", message: e.localizedDescription, preferredStyle: .alert)
+                    let okAction = UIAlertAction.init(title: "Ok", style: .default)
+                    alert.addAction(okAction)
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    // Navigate to ChatVC
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
